7a568fd12296b7a66c4721217f60abade4bffc6d
NitaMarius10 <testm95130226@gmail.com>
2025-05-28 12:31:33 +0300

    small updates

---
+++ rewrite-authors.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="roman.demetra21@gmail.com"
+CORRECT_NAME="NitaMarius10"
+CORRECT_EMAIL="testm95130226@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
